74469a16e8f9558d5a0c8c4f7ac1cd3e079c925a
vpotineni1 <116973063+vpotineni1@users.noreply.github.com>
2025-11-06 18:37:10 -0600

    Finished HW4

--- cloudysky/cloudysky_app/urls.py
+++ cloudysky/cloudysky_app/urls.py
@@ -1,9 +1,12 @@
 
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("time", views.time_central, name="time"),
     path("sum", views.sum, name="sum"),
+    path("", views.index, name = "index"),
+    path('login', views.login_new, name="login_new"),
+    path("new", views.new, name="new_user"),
+    path("createUser", views.createUser, name="createUser"),
 ]
--- cloudysky/cloudysky_app/views.py
+++ cloudysky/cloudysky_app/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-from datetime import datetime
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
 from zoneinfo import ZoneInfo
+import datetime
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+
+
+
 
 def time_central(request):
     if request.method == "GET": 
-        time_CDT = datetime.now(ZoneInfo("America/Chicago"))
+        time_CDT = datetime.datetime.now(ZoneInfo("America/Chicago"))
         return HttpResponse(time_CDT.strftime("%H:%M"), status = 200)
 
 def sum(request):
@@ -17,3 +24,51 @@ def sum(request):
         n2 = request.GET.get('n2')
         sum = float(n1) + float(n2)
         return HttpResponse(str(sum), status = 200)
+    
+def index(request):
+    current_time = datetime.datetime.now(ZoneInfo("America/Chicago"))
+    time_str = current_time.strftime('%H:%M')
+    return render(request, 'app/index.html', context={'current_time':time_str})
+
+def new(request):
+    if request.method != "GET":
+        return HttpResponseNotAllowed(["GET"])
+    return render(request, "app/new.html")
+
+@csrf_exempt
+def createUser(request):
+    if request.method != "POST":
+        return HttpResponseNotAllowed(["POST"])
+    email = request.POST.get("email", "")
+    username = request.POST.get("user_name", "")
+    password = request.POST.get("password", "")
+    is_admin = request.POST.get("is_admin", "0")
+
+    if User.objects.filter(email = email).exists():
+        return HttpResponse("Error: Email in Use", status = 400)
+    
+    user = User.objects.create_user(username=username, email=email, password=password)
+    if is_admin == '1':
+        user.is_staff = True
+        user.is_superuser = True
+    user.save()
+    return redirect('login_new')
+
+def login_new(request):
+    if request.method == "POST":
+        username = request.POST.get("user_name", "")
+        password = request.POST.get("password", "")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("index")
+        else:
+            return render(request, 'registration/login.html',{'error': "Invalid username or password"})
+    else:
+        return render(request, 'registration/login.html')
+
+        
+
+
+
+    
