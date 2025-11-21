2028dea9069eb828f5d85049105e6d9cbd323625
vpotineni1 <116973063+vpotineni1@users.noreply.github.com>
2025-11-20 18:29:33 -0600

    HW6

--- puzzle.py
+++ puzzle.py
@@ -1,4 +1,4 @@
-puzzle_easy_key = "6346"
-puzzle_easy_misspell = "Fabruart,"
+puzzle_easy_key = "5262"
+puzzle_easy_misspell = "extraprdinrry"
 puzzle_key = "049677629"
 puzzle_misspell = "tysxnt." 
